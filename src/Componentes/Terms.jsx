4a91d0565d137525bceb4f80e170dcd3e42d889e
Richard Reyes <rrrv01@gmail.com>
2024-03-13 20:53:47 -0300

    Update Terms.jsx

--- src/Componentes/Terms.jsx
+++ src/Componentes/Terms.jsx
@@ -5,11 +5,9 @@ import TermsCS2 from './Terms2'
 import TermsCS3 from './Terms3'
 import TermsCS4 from './terms4'
 import TermsCS5 from './Terms5'
-<<<<<<< HEAD
 import TermsCS6 from './Terms6'
-=======
 import TermsCS7 from './terms7'
->>>>>>> a2c9e7bddbceca0f7123cb0bc7c61da114c58ceb
+import TermsCS8 from './terms8'
 
 
 
@@ -23,11 +21,10 @@ export default function Terms() {
      <TermsCS3/>
      <TermsCS4/>
      <TermsCS5/>
-<<<<<<< HEAD
      <TermsCS6/>
-=======
      <TermsCS7/>
->>>>>>> a2c9e7bddbceca0f7123cb0bc7c61da114c58ceb
+     <TermsCS8/>
+
     </div>
     </>
   )
