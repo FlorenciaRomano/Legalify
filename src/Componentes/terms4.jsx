7cd432a5051d506fa834ca0721487a4537fcf8da
Richard Reyes <rrrv01@gmail.com>
2024-03-13 19:21:13 -0300

    Update terms4.jsx

--- src/Componentes/terms4.jsx
+++ src/Componentes/terms4.jsx
@@ -2,68 +2,71 @@ import React from 'react'
 import {useTranslation, withTranslation, Trans} from 'react-i18next'
 import {Box, Container, Typography} from '@mui/material'
 import LogoTC from '../assets/logo_legalify_azul_transparente.png'
-import Principal from '../assets/TerminosYcondiciones/Seccion3Derecha.png'
+import Principal from '../assets/TerminosYcondiciones/dataBase.png'
+import contentImg from '../assets/TerminosYcondiciones/section4.png'
 
 export default function TermsCS4() {
   const {t, i18n} = useTranslation()
   return (
-    <>
-      <Box sx={{display: 'flex', width: '100%', justifyContent: 'center'}}>
-        <div>
-          <Box sx={{height:300}} >
+    <Box sx={{display: 'flex', width: '96%', ml: 4, justifyContent: 'center'}}>
+      <Box>
+        <Box sx={{height: 300}}>
+          <Typography variant='h4' sx={{textDecoration:'underline'}}>
             <Trans i18nKey='terms4.t1' />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-          </Box>
-          <Box sx={{height:300}} >
-            <Trans i18nKey='terms4.t2' />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-            <img src={LogoTC} alt='' width={100} />
-          </Box>
-        </div>
-        <Box>
-          <div>
-            <ul>
-              {t('terms4.ls1', {returnObjects: true}).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <ul>
-              {t('terms4.ls2', {returnObjects: true}).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <ul>
-              {t('terms4.ls3', {returnObjects: true}).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <ul>
-              {t('terms4.ls4', {returnObjects: true}).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </div>
-        </Box>
-        <div className='PoliticaDePrivacidad'>
-          <img src={Principal} alt='' />
-          <Typography variant='h2'>
-            <Trans i18nKey='terms4.blue' />
           </Typography>
-        </div>
+          <img src={contentImg} alt='Organización de datos' width={400} />
+        </Box>
+        <Box sx={{height: 300}}>
+          <Typography variant='h4' sx={{textDecoration:'underline'}}>
+            <Trans i18nKey='terms4.t2' />
+          </Typography>
+          <img src={contentImg} alt='Organización de datos' width={400} />
+        </Box>
       </Box>
-    </>
+      <Box>
+        <Box>
+          <ul>
+            {t('terms4.ls1', {returnObjects: true}).map((item, index) => (
+              <li className='textListSec4' key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </Box>
+        <Box sx={{mt: 2}}>
+          <ul>
+            {t('terms4.ls2', {returnObjects: true}).map((item, index) => (
+              <li className='textListSec4' key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </Box>
+        <Box sx={{mt: 4}}>
+          <ul>
+            {t('terms4.ls3', {returnObjects: true}).map((item, index) => (
+              <li className='textListSec4' key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </Box>
+        <Box sx={{mt: 4}}>
+          <ul>
+            {t('terms4.ls4', {returnObjects: true}).map((item, index) => (
+              <li className='textListSec4' key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </Box>
+      </Box>
+      <Box className='PoliticaDePrivacidad'>
+        <img src={Principal} alt='' />
+        <Typography variant='h2'>
+          <Trans i18nKey='terms4.blue' />
+        </Typography>
+      </Box>
+    </Box>
   )
 }
