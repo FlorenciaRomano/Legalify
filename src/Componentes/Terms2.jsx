8e436b33f5dc7fd60efaa3a071b6a01c4ed7f201
FlorenciaRomano <florromanoyt@gmail.com>
2024-03-12 21:01:50 -0300

    modificaciones

--- src/Componentes/Terms2.jsx
+++ src/Componentes/Terms2.jsx
@@ -3,6 +3,7 @@ import LogoTC from '../assets/logo_legalify_azul_transparente.png'
 import img1 from '../assets/TerminosYcondiciones/ImagenS2.png'
 import Info from '../assets/TerminosYcondiciones/info.png'
 import Candado from '../assets/TerminosYcondiciones/candado.png'
+import PoliticaDePrivacidad from '../assets/TerminosYcondiciones/PoliticaDePrivacidad.png'
 
 const TermsCS2 = () => {
     return (
@@ -26,12 +27,13 @@ const TermsCS2 = () => {
                     <img src={Candado} alt="" />
 
                 </div>
+                <div>
                 <p>
                     Es una plataforma digital que pone en contacto a personas que necesitan asesoramiento
                     jurídico (los "clientes") con abogados especializados que pueden proporcionar dicho
                     asesoramiento (los "asesores jurídicos").
                 </p>
-
+                  
                 <p> Para prestar adecuadamente este servicio, necesitamos recabar información personal de
                     nuestros clientes y asesores jurídicos (los "usuarios").
                 </p>
@@ -43,6 +45,12 @@ const TermsCS2 = () => {
                     Esta política sigue todas las leyes aplicables, incluido el Reglamento General Europeo
                     de Protección de Datos (RGPD).
                 </p>
+                </div>
+
+                <div className='PoliticaDePrivacidad'>
+                    <img src={PoliticaDePrivacidad} alt="" />
+                    <h2>POLITICA DE PRIVACIDAD</h2>
+                </div>
             </section>
            
         </>
