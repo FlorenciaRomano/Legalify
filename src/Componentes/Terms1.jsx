5aba79d06b9de5f686fec1a576de943431ea3645
Richard Reyes <rrrv01@gmail.com>
2024-03-13 14:24:50 -0300

    Update Terms1.jsx

--- src/Componentes/Terms1.jsx
+++ src/Componentes/Terms1.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import '../styles/Index.css';
+import React from 'react'
+import '../styles/Index.css'
 import LogoTC from '../assets/logo_legalify_azul_transparente.png'
-
-
-
+import {useTranslation, withTranslation, Trans} from 'react-i18next'
+import {Typography} from '@mui/material'
 
 function TermCS1() {
-    return (
-        <div>
+  const {t, i18n} = useTranslation()
 
-            <div className='TermCS1'>
-                <img src={LogoTC} alt="" width={250} />
-                <h1>Nuestra política de privacidad de datos personales en español </h1>
-            </div>
-
-
-
-
-        </div>
-
-    )
+  return (
+    <div>
+      <div className='TermCS1'>
+        <img src={LogoTC} alt='' width={250} />
+        <Typography variant='h2' component='h2' align='center'>
+          <Trans i18nKey='title' />
+        </Typography>
+      </div>
+    </div>
+  )
 }
 export default TermCS1
-
-
