6692ecdb46413d7426baf8e4e11c5e4f99c17132
Richard Reyes <rrrv01@gmail.com>
2024-03-13 14:24:34 -0300

    Update Terms.jsx

--- src/Componentes/Terms.jsx
+++ src/Componentes/Terms.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import TermCS1 from './Terms1'
 import TermsCS2 from './Terms2'
 import TermsCS3 from './Terms3'
+import TermsCS4 from './terms4'
 
 export default function Terms() {
   return (
@@ -12,6 +13,7 @@ export default function Terms() {
      <TermCS1/>
      <TermsCS2/>
      <TermsCS3/>
+     <TermsCS4/>
     </div>
     </>
   )
