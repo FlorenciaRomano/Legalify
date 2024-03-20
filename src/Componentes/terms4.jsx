006dff301534d655e06b11193e0d637a5e92c0b4
Richard Reyes <rrrv01@gmail.com>
2024-03-19 21:41:10 -0400

    Update terms4.jsx

--- src/Componentes/terms4.jsx
+++ src/Componentes/terms4.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import {useTranslation, Trans} from 'react-i18next'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, useMediaQuery} from '@mui/material'
 import LogoTC from '../assets/logo_legalify_azul_transparente.png'
 import Principal from '../assets/TerminosYcondiciones/dataBase.png'
 import contentImg from '../assets/TerminosYcondiciones/section4.png'
 
 export default function TermsCS4() {
   const {t, i18n} = useTranslation()
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   return (
     <Box sx={{display: 'flex', width: '96%', ml: 4, justifyContent: 'center'}}>
       <Box>
-        <Box sx={{height: 300}}>
-          <Typography variant='h4' sx={{textDecoration: 'underline'}}>
+        <Box sx={{height: 300, bgcolor: 'blue'}}>
+          <Typography variant='h4' sx={{textDecoration: 'underline', width: '50vw', bgcolor: 'red'}}>
             <Trans i18nKey='terms4.t1' />
           </Typography>
           <img src={contentImg} alt='Organización de datos' width={400} />
@@ -61,18 +63,15 @@ export default function TermsCS4() {
           </ul>
         </Box>
       </Box>
-      <Box className='PoliticaDePrivacidad'>
-
-        <img src={Principal} alt='' width={150} />
-        <Typography variant='h2'/>
-        
-
-      
-
-        <h2>
-          <Trans i18nKey='terms4.blue' />
-        </h2>
-      </Box>
+      {isMobile ? null : (
+        <Box className='PoliticaDePrivacidad'>
+          <img src={Principal} alt='' width={150} />
+          <Typography variant='h2' />
+          <h2>
+            <Trans i18nKey='terms4.blue' />
+          </h2>
+        </Box>
+      )}
     </Box>
   )
 }
