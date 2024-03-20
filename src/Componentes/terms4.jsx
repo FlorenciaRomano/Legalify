ce34208b15f943dbb8fbd13c8b463f9430ae41e7
Richard Reyes <rrrv01@gmail.com>
2024-03-19 21:59:09 -0400

    Update terms4.jsx

--- src/Componentes/terms4.jsx
+++ src/Componentes/terms4.jsx
@@ -12,56 +12,114 @@ export default function TermsCS4() {
   return (
     <Box sx={{display: 'flex', width: '96%', ml: 4, justifyContent: 'center'}}>
       <Box>
-        <Box sx={{height: 300, bgcolor: 'blue'}}>
-          <Typography variant='h4' sx={{textDecoration: 'underline', width: '50vw', bgcolor: 'red'}}>
-            <Trans i18nKey='terms4.t1' />
-          </Typography>
-          <img src={contentImg} alt='Organización de datos' width={400} />
-        </Box>
-        <Box sx={{height: 300}}>
-          <Typography variant='h4' sx={{textDecoration: 'underline'}}>
-            <Trans i18nKey='terms4.t2' />
-          </Typography>
-          <img src={contentImg} alt='Organización de datos' width={400} />
-        </Box>
-      </Box>
-      <Box>
-        <Box>
-          <ul>
-            {t('terms4.ls1', {returnObjects: true}).map((item, index) => (
-              <li className='textListSec4' key={index}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        </Box>
-        <Box sx={{mt: 2}}>
-          <ul>
-            {t('terms4.ls2', {returnObjects: true}).map((item, index) => (
-              <li className='textListSec4' key={index}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        </Box>
-        <Box sx={{mt: 4}}>
-          <ul>
-            {t('terms4.ls3', {returnObjects: true}).map((item, index) => (
-              <li className='textListSec4' key={index}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        </Box>
-        <Box sx={{mt: 4}}>
-          <ul>
-            {t('terms4.ls4', {returnObjects: true}).map((item, index) => (
-              <li className='textListSec4' key={index}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        </Box>
+        {isMobile ? (
+          <>
+            <Box>
+              <Box sx={{height: 300, width:'95vw', bgcolor: 'blue'}}>
+                <Typography variant='h4' sx={{textDecoration: 'underline', bgcolor: 'red'}}>
+                  <Trans i18nKey='terms4.t1' />
+                </Typography>
+                <img src={contentImg} alt='Organización de datos' width={350} />
+              </Box>
+              
+            </Box>
+            <Box>
+              <ul>
+                {t('terms4.ls1', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box sx={{mt: 2}}>
+              <ul>
+                {t('terms4.ls2', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box sx={{height: 300}}>
+                <Typography variant='h4' sx={{textDecoration: 'underline'}}>
+                  <Trans i18nKey='terms4.t2' />
+                </Typography>
+                <img src={contentImg} alt='Organización de datos' width={350} />
+              </Box>
+            <Box sx={{mt: 4}}>
+              <ul>
+                {t('terms4.ls3', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box sx={{mt: 4}}>
+              <ul>
+                {t('terms4.ls4', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+          </>
+        ) : (
+          <>
+            <Box>
+              <Box sx={{height: 300, bgcolor: 'blue'}}>
+                <Typography variant='h4' sx={{textDecoration: 'underline', bgcolor: 'red'}}>
+                  <Trans i18nKey='terms4.t1' />
+                </Typography>
+                <img src={contentImg} alt='Organización de datos' width={400} />
+              </Box>
+              <Box sx={{height: 300}}>
+                <Typography variant='h4' sx={{textDecoration: 'underline'}}>
+                  <Trans i18nKey='terms4.t2' />
+                </Typography>
+                <img src={contentImg} alt='Organización de datos' width={400} />
+              </Box>
+            </Box>
+            <Box>
+              <ul>
+                {t('terms4.ls1', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box sx={{mt: 2}}>
+              <ul>
+                {t('terms4.ls2', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box sx={{mt: 4}}>
+              <ul>
+                {t('terms4.ls3', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box sx={{mt: 4}}>
+              <ul>
+                {t('terms4.ls4', {returnObjects: true}).map((item, index) => (
+                  <li className='textListSec4' key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+          </>
+        )}
       </Box>
       {isMobile ? null : (
         <Box className='PoliticaDePrivacidad'>
