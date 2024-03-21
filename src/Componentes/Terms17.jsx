9116107f3c37df8a29b913e3d4ab822c31c7164c
Richard Reyes <rrrv01@gmail.com>
2024-03-20 22:20:55 -0400

    Update Terms17.jsx

--- src/Componentes/Terms17.jsx
+++ src/Componentes/Terms17.jsx
@@ -1,29 +1,28 @@
 import React from 'react'
 import Item from '../assets/TerminosYcondiciones/Seccion17Derecha.png'
-
+import {useMediaQuery} from '@mui/material'
 
 const TermsCS17 = () => {
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   return (
     <>
       <div className='ContenedorGeneralS3'>
         <div className='ContenedorMedioS14'>
           <section className='Seccion3TC'>
             <div className='itemsP15'>
-                <img src={Item} alt="" width={250} />
+              <img src={Item} alt='' width={250} />
               <span>
-              <p>Si tiene alguna pregunta sobre esta Política, escríbanos a la siguiente dirección 
-           de correo electrónico: info@legalify.app </p>
-              
-              </span> 
-
+                <p>Si tiene alguna pregunta sobre esta Política, escríbanos a la siguiente dirección de correo electrónico: info@legalify.app </p>
+              </span>
             </div>
           </section>
         </div>
-
-        <div className='PoliticaDePrivacidad'>
-          
-          <h2>CONTACTO</h2>
-        </div>
+        {isMobile ? null : (
+          <div className='PoliticaDePrivacidad'>
+            <h2>CONTACTO</h2>
+          </div>
+        )}
       </div>
     </>
   )
