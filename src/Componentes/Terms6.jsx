df06f969dfce81468bb941a937784eb6b625e73b
Richard Reyes <rrrv01@gmail.com>
2024-03-20 22:20:58 -0400

    Update Terms6.jsx

--- src/Componentes/Terms6.jsx
+++ src/Componentes/Terms6.jsx
@@ -4,7 +4,7 @@ import Item1 from '../assets/TerminosYcondiciones/Seccion6Item1.png'
 import Item2 from '../assets/TerminosYcondiciones/Seccion6Item2.png'
 import Item3 from '../assets/TerminosYcondiciones/Seccion6Item3.png'
 import Principal from '../assets/TerminosYcondiciones/Seccion6Derecha.png'
-import {useMediaQuery} from '@mui/material'
+import {Box, useMediaQuery} from '@mui/material'
 
 const TermsCS6 = () => {
   const isMobile = useMediaQuery('(max-width: 768px)')
@@ -18,25 +18,25 @@ const TermsCS6 = () => {
           </div>
 
           <section className='Seccion3TC'>
-            <div className='itemsS6'>
-              <img src={Item1} alt='' />
-              <img src={Item2} alt='' />
-              <img src={Item3} alt='' />
-            </div>
-            <div className='itemsP3'>
+            <Box className='itemsP3' sx={{width:'90%'}}>
               <ul>
-                <li>No se hace responsable de las pérdidas o daños que puedan producirse si usted incumple la obligación de proporcionar información adecuada.</li>
-
-                <li>No tiene control sobre el tipo o la cantidad de información que se intercambia en nuestra plataforma.</li>
-
-                <li>No se hace responsable del uso que los asesores jurídicos hagan de los datos personales facilitados por los clientes una vez se han conectado a través de nuestra plataforma</li>
+                <Box sx={{display: 'flex', gap:5}}>
+                  <img src={Item1} alt='' />
+                  <li>No se hace responsable de las pérdidas o daños que puedan producirse si usted incumple la obligación de proporcionar información adecuada.</li>
+                </Box>
+                <Box sx={{display: 'flex', gap:5}}>
+                  <img src={Item2} alt='' /> <li>No tiene control sobre el tipo o la cantidad de información que se intercambia en nuestra plataforma.</li>
+                </Box>
+                <Box sx={{display: 'flex', gap:5}}>
+                  <img src={Item3} alt='' /> <li>No se hace responsable del uso que los asesores jurídicos hagan de los datos personales facilitados por los clientes una vez se han conectado a través de nuestra plataforma</li>
+                </Box>
               </ul>
               <p>Es su responsabilidad asegurarse de que la información que proporciona es verdadera, completa, precisa y actualizada.</p>
 
               <p>Si utilizas o compartes datos personales de otros usuarios o de terceros (incluidos menores de edad), debes contar previamente con su consentimiento. Este consentimiento debe ser libre, previo, expreso e inequívoco.</p>
 
               <p>Si consideras que determinados contenidos de nuestra web vulneran tu privacidad o la de otro tienes la opción de escribirnos a: info@legalify.app</p>
-            </div>
+            </Box>
           </section>
         </div>
 
