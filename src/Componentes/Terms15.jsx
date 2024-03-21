3217a1581a5003e5b5c64f3fb5dd4dd013c763f4
Richard Reyes <rrrv01@gmail.com>
2024-03-20 22:20:48 -0400

    Update Terms15.jsx

--- src/Componentes/Terms15.jsx
+++ src/Componentes/Terms15.jsx
@@ -1,40 +1,36 @@
 import React from 'react'
 import Item from '../assets/TerminosYcondiciones/Seccion15Item.png'
 import Principal from '../assets/TerminosYcondiciones/Seccion15Derecha.png'
+import {useMediaQuery} from '@mui/material'
 
 const TermsCS15 = () => {
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   return (
     <>
       <div className='ContenedorGeneralS3'>
         <div className='ContenedorMedioS14'>
           <section className='Seccion3TC'>
             <div className='itemsP15'>
-                <img src={Item} alt="" width={200} />
+              <img src={Item} alt='' width={200} />
               <span>
-              <p>Al aceptar esta Política, usted nos da su consentimiento:</p>
-              <ul>
-                <li>
-                    Libre
-                </li>
-                <li>
-                    Expreso
-                </li>
-                <li>
-                Inequívoco e informado.
-                </li>
-              </ul>
-              <p>Esto con la finalidad de tratar sus datos personales de acuerdo 
-                con los fines expuestos anteriormente (artículo 4).</p>
-              </span> 
-
+                <p>Al aceptar esta Política, usted nos da su consentimiento:</p>
+                <ul>
+                  <li>Libre</li>
+                  <li>Expreso</li>
+                  <li>Inequívoco e informado.</li>
+                </ul>
+                <p>Esto con la finalidad de tratar sus datos personales de acuerdo con los fines expuestos anteriormente (artículo 4).</p>
+              </span>
             </div>
           </section>
         </div>
-
-        <div className='PoliticaDePrivacidad'>
-          <img src={Principal} alt='' />
-          <h2>CONSENTIMIENTO</h2>
-        </div>
+        {isMobile ? null : (
+          <div className='PoliticaDePrivacidad'>
+            <img src={Principal} alt='' />
+            <h2>CONSENTIMIENTO</h2>
+          </div>
+        )}
       </div>
     </>
   )
