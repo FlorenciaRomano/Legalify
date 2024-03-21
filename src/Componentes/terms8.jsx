c03f37cfab2455dd1a53da8d7603a3977fd2fadc
Richard Reyes <rrrv01@gmail.com>
2024-03-20 22:21:05 -0400

    Update terms8.jsx

--- src/Componentes/terms8.jsx
+++ src/Componentes/terms8.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useTranslation, Trans} from 'react-i18next'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, useMediaQuery} from '@mui/material'
 import Principal from '../assets/TerminosYcondiciones/section8.png'
 import Logo from '../assets/TerminosYcondiciones/LegalifyLogo.png'
 import item1 from '../assets/TerminosYcondiciones/s8i1.png'
@@ -10,8 +10,10 @@ import item4 from '../assets/TerminosYcondiciones/s8i4.png'
 
 export default function TermsCS8() {
   const {t, i18n} = useTranslation()
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   return (
-    <Box sx={{mt: 4, display: 'flex', ml: 1, justifyContent: 'center'}}>
+    <Box sx={{mt: 4, display: 'flex', ml: 1, justifyContent: 'center', width:{sm:'80vw',md:'80%'}}}>
       <Box>
         <Box>
           <img src={Logo} width={130} alt={<Trans i18nKey='terms8.item1' />} />
@@ -22,25 +24,25 @@ export default function TermsCS8() {
           </Typography>
         </Box>
         <Box sx={{display: 'flex', flexDirection: 'column'}}>
-          <Box sx={{mt:4}} >
+          <Box sx={{mt: 4}}>
             <img src={item1} width={100} alt={<Trans i18nKey='terms8.item1' />} />
             <Typography variant='p'>
               <Trans i18nKey='terms8.item1' />
             </Typography>
           </Box>
-          <Box sx={{mt:4}} >
+          <Box sx={{mt: 4}}>
             <img src={item2} width={100} alt={<Trans i18nKey='terms8.item2' />} />
             <Typography variant='p'>
               <Trans i18nKey='terms8.item2' />
             </Typography>
           </Box>
-          <Box sx={{mt:4}} >
+          <Box sx={{mt: 4}}>
             <img src={item3} width={100} alt={<Trans i18nKey='terms8.item3' />} />
             <Typography variant='p'>
               <Trans i18nKey='terms8.item3' />
             </Typography>
           </Box>
-          <Box sx={{mt:4}} >
+          <Box sx={{mt: 4}}>
             <img src={item4} width={100} alt={<Trans i18nKey='terms8.item4' />} />
             <Typography variant='p'>
               <Trans i18nKey='terms8.item4' />
@@ -48,12 +50,14 @@ export default function TermsCS8() {
           </Box>
         </Box>
       </Box>
-      <Box className='PoliticaDePrivacidad'>
-        <img src={Principal} width={150} alt={<Trans i18nKey='terms7.blue' />} />
-        <h2>
-          <Trans i18nKey='terms8.blue' />
-        </h2>
-      </Box>
+      {isMobile ? null : (
+        <Box className='PoliticaDePrivacidad'>
+          <img src={Principal} width={150} alt={<Trans i18nKey='terms7.blue' />} />
+          <h2>
+            <Trans i18nKey='terms8.blue' />
+          </h2>
+        </Box>
+      )}
     </Box>
   )
 }
